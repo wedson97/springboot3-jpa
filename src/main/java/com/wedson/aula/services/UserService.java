be7bf9c2704717f6b0a62ec3e8087d2b447d4fbd
Wedson <wedsonsilva025@gmail.com>
2023-08-17 10:01:28 -0300

    exception handling - update

--- src/main/java/com/wedson/aula/services/UserService.java
+++ src/main/java/com/wedson/aula/services/UserService.java
@@ -13,6 +13,8 @@ import com.wedson.aula.repositories.UserRepository;
 import com.wedson.aula.services.exceptions.DatabaseException;
 import com.wedson.aula.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 
 @Service
 public class UserService {
@@ -45,9 +47,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
