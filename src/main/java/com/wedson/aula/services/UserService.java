9d521f5317a21c8746b750c9dca09bb85d48cb91
Wedson <wedsonsilva025@gmail.com>
2023-08-17 09:45:31 -0300

    Exception handling - findById

--- src/main/java/com/wedson/aula/services/UserService.java
+++ src/main/java/com/wedson/aula/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.wedson.aula.entities.User;
 import com.wedson.aula.repositories.UserRepository;
+import com.wedson.aula.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
